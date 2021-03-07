7a33eb069bc829709fe88f301a14c62ef1bd97da
DerRafDev <rafael.sordi.lino@gmail.com>
2021-03-07 10:59:59 -0400

    First Commit
    Installing React

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>CRYPTOCURRENCY TRACKER</h1>
+    </div>
+  );
+}
+
+export default App;
